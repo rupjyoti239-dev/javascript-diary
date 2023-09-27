bafaea2b6fc6a4b90f872ac74e13543deb934c37
Rupjyoti Sarma <rup.sarma23@gmail.com>
2023-09-27 22:15:41 +0530

    stirage & hoisting

---
+++ 13_hoisting/index.js
@@ -0,0 +1,39 @@
+console.log(x);
+var x = 7;
+
+// console.log(name);
+// let name = "rup"
+
+
+
+
+
+// getName()
+// console.log(getName);
+// function getName(){
+//   console.log("hello rup");
+// }
+
+
+let nm = function(x){
+  console.log(x);
+}
+
+console.log(nm(455));
+
+
+
+// clouser
+function x(){
+  let a = 7;
+  function y(){
+    console.log(a);
+  }
+  y()
+}
+x()
+
+
+
+
+
