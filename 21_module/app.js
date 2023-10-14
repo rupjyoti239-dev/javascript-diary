3708453bc834927589a29f33b773bde536310215
Rupjyoti Sarma <rup.sarma23@gmail.com>
2023-10-14 15:29:30 +0530

    module

---
+++ 21_module/app.js
@@ -0,0 +1,3 @@
+import { name,title } from "./dir/name.js";
+import { age } from "./dir/age.js";
+console.log(`${name} ${title} and ${age}`);
---
+++ 21_module/dir/name.js
@@ -0,0 +1,3 @@
+const name  = "rupjyoti";
+const title = "sarma";
+export {name,title}
